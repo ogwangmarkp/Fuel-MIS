f09e05b459b0d3c5522d4ebf661a87572f0b190e
Mark Ogwang Paul <Mark.Ogwang@centenarygroup.co.ug>
2025-04-04 14:06:35 +0300

    recent changes

--- kwani_api/settings.py
+++ kwani_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY', default='')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','13.53.40.163','fuel.tracesms.co']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','13.53.40.163','fuelapi.tracesms.co']
 
 # Application definition
 INSTALLED_APPS = [
